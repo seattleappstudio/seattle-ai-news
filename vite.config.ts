24d81e884b0c7421f5aaa595586dcc81ef2ae098
Joseph Perez <josmperez@protonmail.com>
2025-06-12 12:26:11 -0700

    Add base to vite file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./" // or "/" depending on needs
   optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  exclude: ['lucide-react'],
+})
